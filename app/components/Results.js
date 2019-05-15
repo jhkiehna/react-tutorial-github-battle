b21c5ee5b96b092df03845621023a1bf66664813
jhkiehna <jhkiehna@gmail.com>
2019-05-15 14:23:03 -0400

    refactoring, added Card component

--- app/components/Results.js
+++ app/components/Results.js
@@ -8,6 +8,7 @@ import {
   FaCode,
   FaUser
 } from "react-icons/fa";
+import Card from "./Card";
 
 export default class Results extends React.Component {
   constructor(props) {
@@ -54,26 +55,13 @@ export default class Results extends React.Component {
 
     return (
       <div className="grid space-around container-sm">
-        <div className="card bg-light">
-          <h4 className="header-lg center-text">
-            {winner.score === loser.score ? "Tie" : "Winner"}
-          </h4>
-
-          <h4 className="center-text">
-            Score: {winner.score.toLocaleString()}
-          </h4>
-
-          <img
-            className="avatar"
-            src={winner.profile.avatar_url}
-            alt={`Avatar for ${winner.profile.login}`}
-          />
-          <h2 className="center-text">
-            <a className="link" href={winner.profile.html_url}>
-              {winner.profile.login}
-            </a>
-          </h2>
-
+        <Card
+          header={winner.score === loser.score ? "Tie" : "Winner"}
+          subheader={`Score: ${winner.score.toLocaleString()}`}
+          avatar={winner.profile.avatar_url}
+          href={winner.profile.html_url}
+          name={winner.profile.login}
+        >
           <ul className="card-list">
             <li>
               <FaUser color="rgb(239, 115, 115)" size={22} />
@@ -109,25 +97,15 @@ export default class Results extends React.Component {
               {JSON.stringify(winner.profile.public_repos)} repos
             </li>
           </ul>
-        </div>
-        <div className="card bg-light">
-          <h4 className="header-lg center-text">
-            {winner.score === loser.score ? "Tie" : "Loser"}
-          </h4>
+        </Card>
 
-          <h4 className="center-text">Score: {loser.score.toLocaleString()}</h4>
-
-          <img
-            className="avatar"
-            src={loser.profile.avatar_url}
-            alt={`Avatar for ${loser.profile.login}`}
-          />
-          <h2 className="center-text">
-            <a className="link" href={loser.profile.html_url}>
-              {loser.profile.login}
-            </a>
-          </h2>
-
+        <Card
+          header={winner.score === loser.score ? "Tie" : "Winner"}
+          subheader={`Score: ${loser.score.toLocaleString()}`}
+          avatar={loser.profile.avatar_url}
+          href={loser.profile.html_url}
+          name={loser.profile.login}
+        >
           <ul className="card-list">
             <li>
               <FaUser color="rgb(239, 115, 115)" size={22} />
@@ -163,7 +141,7 @@ export default class Results extends React.Component {
               {JSON.stringify(loser.profile.public_repos)} repos
             </li>
           </ul>
-        </div>
+        </Card>
       </div>
     );
   }
