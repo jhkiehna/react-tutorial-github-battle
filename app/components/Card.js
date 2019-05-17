567dee1727ce1fd0fe7e04f4c523dd0a3e5d76a1
jhkiehna <jhkiehna@gmail.com>
2019-05-17 13:12:10 -0400

    adding themes

--- app/components/Card.js
+++ app/components/Card.js
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { ThemeConsumer } from "../context/theme";
 
 export default function Card({
   header,
@@ -10,20 +11,24 @@ export default function Card({
   children
 }) {
   return (
-    <div className="card bg-light">
-      <h4 className="header-lg center-text">{header}</h4>
+    <ThemeConsumer>
+      {({ theme }) => (
+        <div className={`card bg-${theme}`}>
+          <h4 className="header-lg center-text">{header}</h4>
 
-      {subheader && <h4 className="center-text">{subheader}</h4>}
+          {subheader && <h4 className="center-text">{subheader}</h4>}
 
-      <img className="avatar" src={avatar} alt={`Avatar for ${name}`} />
-      <h2 className="center-text">
-        <a className="link" href={href}>
-          {name}
-        </a>
-      </h2>
+          <img className="avatar" src={avatar} alt={`Avatar for ${name}`} />
+          <h2 className="center-text">
+            <a className="link" href={href}>
+              {name}
+            </a>
+          </h2>
 
-      {children}
-    </div>
+          {children}
+        </div>
+      )}
+    </ThemeConsumer>
   );
 }
 
