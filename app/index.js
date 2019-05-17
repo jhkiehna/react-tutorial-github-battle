a0d97655fbffcced2770b03ac4202d1516c26f33
jhkiehna <jhkiehna@gmail.com>
2019-05-17 12:45:48 -0400

    added nav component

--- app/index.js
+++ app/index.js
@@ -4,9 +4,10 @@ import "./index.css";
 import Popular from "./components/Popular";
 import Battle from "./components/Battle";
 import { ThemeProvider } from "./context/theme";
+import Nav from "./components/Nav";
 
 class App extends React.Component {
-  constructor() {
+  constructor(props) {
     super(props);
 
     this.state = {
@@ -22,9 +23,12 @@ class App extends React.Component {
   render() {
     return (
       <ThemeProvider value={this.state}>
-        <div className="container">
-          <Popular />
-          <Battle />
+        <div className={this.state.theme}>
+          <div className="container">
+            <Nav />
+            <Popular />
+            <Battle />
+          </div>
         </div>
       </ThemeProvider>
     );
