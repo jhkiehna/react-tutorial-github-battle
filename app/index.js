9ab3ea01d7323fd4ce5b087124eb34080a8890ff
jhkiehna <jhkiehna@gmail.com>
2019-05-17 12:34:36 -0400

    adding context for theme

--- app/index.js
+++ app/index.js
@@ -3,14 +3,30 @@ import ReactDom from "react-dom";
 import "./index.css";
 import Popular from "./components/Popular";
 import Battle from "./components/Battle";
+import { ThemeProvider } from "./context/theme";
 
 class App extends React.Component {
+  constructor() {
+    super(props);
+
+    this.state = {
+      theme: "light",
+      toggleTheme: () => {
+        this.setState(({ theme }) => ({
+          theme: theme === "light" ? "dark" : "light"
+        }));
+      }
+    };
+  }
+
   render() {
     return (
-      <div className="container">
-        <Popular />
-        <Battle />
-      </div>
+      <ThemeProvider value={this.state}>
+        <div className="container">
+          <Popular />
+          <Battle />
+        </div>
+      </ThemeProvider>
     );
   }
 }
