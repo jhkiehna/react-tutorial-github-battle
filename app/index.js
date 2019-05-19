c9fdc068a98538077d17ecdadcfe96e1d5225377
Joseph Kiehna <jhkiehna@gmail.com>
2019-05-19 14:07:53 -0400

    added code split stuff

--- app/index.js
+++ app/index.js
@@ -1,12 +1,17 @@
 import React from "react";
 import ReactDom from "react-dom";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Popular from "./components/Popular";
-import Battle from "./components/Battle";
-import Results from "./components/Results";
+// import Popular from "./components/Popular";
+// import Battle from "./components/Battle";
+// import Results from "./components/Results";
 import Nav from "./components/Nav";
 import "./index.css";
 import { ThemeProvider } from "./context/theme";
+import Loading from "./components/Loading";
+
+const Popular = React.lazy(() => import("./components/Popular"));
+const Battle = React.lazy(() => import("./components/Battle"));
+const Results = React.lazy(() => import("./components/Results"));
 
 class App extends React.Component {
   state = {
@@ -26,12 +31,14 @@ class App extends React.Component {
             <div className="container">
               <Nav />
 
-              <Switch>
-                <Route exact path="/" component={Popular} />
-                <Route exact path="/battle" component={Battle} />
-                <Route path="/battle/results" component={Results} />
-                <Route render={() => <h1>404 not Found</h1>} />
-              </Switch>
+              <React.Suspense fallback={<Loading />}>
+                <Switch>
+                  <Route exact path="/" component={Popular} />
+                  <Route exact path="/battle" component={Battle} />
+                  <Route path="/battle/results" component={Results} />
+                  <Route render={() => <h1>404 not Found</h1>} />
+                </Switch>
+              </React.Suspense>
             </div>
           </div>
         </ThemeProvider>
